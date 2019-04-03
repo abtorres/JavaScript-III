3a64f2b097d79525f1698ef36110df2ac02d43ae
abtorres <abtorres079@gmail.com>
2019-04-02 21:23:40 -0600

    finished first part of this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,16 +1,16 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. global binding - without any context 'this' will refer to the global object. the global object being the window.
+* 2. implicit binding - javascript will look and say that 'this' must be the thing before the dot.
+* 3. new binding - here 'this' refers to whatever object is returned by the constructor function.
+* 4. explicit binding - 'this' is explicit whenever the 'call' or 'apply' methods are used.
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
-
+console.log(this);
 // code example for Window Binding
 
 // Principle 2
